2967f8d02bc462d5f1d9ecff3f4b3205a2be2797
[Daniel] <danieljulian2848@gmail.com>
2023-02-27 13:41:50 -0500

    First commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,12 @@
+/**
+ * Esta función toma la última carta de un arreglo de cartas
+ * @param {Array<String>} deck es un arreglo de string
+ * @returns {String} Regresa la carta del deck
+ */
+export const pedirCarta = (deck) => {
+  if (!deck || deck.length === 0) {
+    throw "No hay cartas en el deck";
+  }
+  const carta = deck.pop();
+  return carta;
+};
